664c617b0197640c08b87d59144fc0e82e956638
hjlee2778 <lhj2778@dongguk.edu>
2024-08-06 17:48:59 +0900

    feat: 웹 페이지 연습

---
+++ ExpressClass/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const port = 3200
+const path = require('path'), asynce = require('async'), fs = require('fs');
+
+const users = [
+    {name: 'kim', email: 'kim@gmail.com'},
+    {name: 'lee', email: 'lee@gmail.com'},
+    {name: 'park', email: 'park@gmail.com'},
+]
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+// app.get('/greet', (req, res) => {
+//     res.send(JSON.stringify({message: "안녕하세요!!!!!"}))
+// }) 
+
+app.get('/users', (req, res) => {
+    res.json(users);
+})
+
+
+app.listen(port, () => {
+    console.log(`서버 실행! http://localhost:${port}`)
+})
