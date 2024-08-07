ae75674db566055c926b13a070e147499d8b4425
hjlee2778 <lhj2778@dongguk.edu>
2024-08-07 09:38:17 +0900

    feat: public 폴더 추가 및 코드 수정

--- ExpressClass/server.js
+++ ExpressClass/server.js
@@ -16,9 +16,10 @@ const users = [
 
 
 
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/index.html'));
+    res.sendFile(path.join(__dirname + 'public'+ 'index.html'));
 })
 
 // app.get('/greet', (req, res) => {
